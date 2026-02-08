04633b6e832157405c40e71e07ffadab8abfd168
Minerva Maravilla <mmarvid1@upv.edu.es>
2026-02-08 22:09:33 +0100

    wasd

--- Assets/Scenes/Personaje.cs
+++ Assets/Scenes/Personaje.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
+    public float vel = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,11 @@ public class Personaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(movement * vel * Time.deltaTime);
     }
 }
