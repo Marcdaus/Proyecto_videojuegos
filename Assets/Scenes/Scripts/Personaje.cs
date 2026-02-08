733b2f322801db750207867d5c27809d7e9df85a
daus_dog <mvmsueca@gmail.com>
2026-02-08 23:32:56 +0100

    agarrar objetos

--- Assets/Scenes/Scripts/Personaje.cs
+++ Assets/Scenes/Scripts/Personaje.cs
@@ -2,42 +2,61 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
-    public float jumpForce = 7f;
-    private Rigidbody rb;
-    private bool isGrounded;
-    // variables
+    // Movimiento
+    public float vel = 5f;
+    public float rotationSpeed = 10f;
 
-    public float vel = 3f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Salto
+    public float jumpForce = 7f;
+    private bool isGrounded;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
     void Start()
     {
-        // asignamos a la variable rb el componenete rigidbody
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Leer input (WASD)
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(h, 0f, v).normalized;
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical);
-        transform.Translate(movement * vel * Time.deltaTime);
-        // detectamos que este en el suelo y que este el espacio pulsado
+        // Rotar hacia donde camina
+        if (movement.magnitude >= 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+        }
+
+        // Salto
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
     }
 
+    void FixedUpdate()
+    {
+        // Mover con físicas
+        rb.MovePosition(rb.position + movement * vel * Time.fixedDeltaTime);
+    }
+
     void Jump()
     {
-        //salto
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isGrounded = false;
     }
-    //comprobar que toque el suelo
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Suelo"))
